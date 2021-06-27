482a85bb1fda38e654f478e423eb6c4eb2f6b59c
kevinchewning <klcbusiness@hotmail.com>
2021-06-27 14:00:45 -0400

    Changed windows to prompts

--- script.js
+++ script.js
@@ -12,46 +12,56 @@ var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var numericChar = "0123456789"
 var specialChar = '!"#$%&()*+,-./:;<=>?@][^_`{|}~'
 
-var lowercase = document.getElementById('lowercase').checked;
-var uppercase = document.getElementById('uppercase').checked;
-var numeric = document.getElementById('numeric').checked;
-var special = document.getElementById('special-char').checked;
+var lowercase
+var uppercase
+var numeric
+var special
 
-var passwordLength = document.getElementById('password-length').value;
+var passwordLength
 
 var password = "";
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", showPrompts);
+generateBtn.addEventListener("click", prompt1);
 
 //Prompts
-function showPrompts() {
-  document.getElementById('prompt-1').style.display = "flex"
-}
+function prompt1() {
+  passwordLength = parseInt(prompt("Enter password length. (Must be between 8 and 128)"));
 
-function submitLength() {
-  passwordLength = document.getElementById('password-length').value;
 
-  if (passwordLength < 8) {
-    alert("Must be between 8 and 128 characters!");
-  } else if (passwordLength > 128) {
-    alert("Must be between 8 and 128 characters!");
+  
+  if (isNaN(passwordLength) || passwordLength == undefined || passwordLength == null) {
+    alert("Please enter a number!");
+    prompt1();
+  } else if (passwordLength < 8 || passwordLength > 128) {
+    alert("Password must be between 8 and 128!");
+    prompt1();
   } else {
-    document.getElementById('prompt-1').style.display = "none";
-    document.getElementById('prompt-2').style.display = "flex"
+    confirm1();
   }
 }
 
-function submitChar() {
-  lowercase = document.getElementById('lowercase').checked;
-  uppercase = document.getElementById('uppercase').checked;
-  numeric = document.getElementById('numeric').checked;
-  special = document.getElementById('special-char').checked;
+function confirm1() {
+  lowercase = confirm("Include lowercase characters?");
+  confirm2();
+}
+
+function confirm2() {
+  uppercase = confirm("Include uppercase characters?");
+  confirm3();
+}
+function confirm3() {
+  numeric = confirm("Include numeric characters?");
+  confirm4();
+}
+
+function confirm4() {
+  special = confirm("Include special characters?");
 
   if (!lowercase && !uppercase && !numeric && !special) {
-    alert("Please choose at least one character type!");
+    alert("You must select at least one type of character set!");
+    confirm1();
   } else {
-    document.getElementById('prompt-2').style.display = "none"
     writePassword();
   }
 }
@@ -64,11 +74,6 @@ function writePassword() {
 function generatePassword() {
   var passwordChar = "";
   password = ""
-
-  lowercase = document.getElementById('lowercase').checked;
-  uppercase = document.getElementById('uppercase').checked;
-  numeric = document.getElementById('numeric').checked;
-  special = document.getElementById('special-char').checked;
 
   lowercase ? (passwordChar += lowercaseChar) : "";
   uppercase ? (passwordChar += uppercaseChar) : "";
